4b51e4cde6ef6f4e7c095cef844c57300bc000bf
BlinKarma <jason_almandroz@hotmail.com>
2019-09-24 07:24:41 -0500

    Agregado consumo tutor

--- app/src/main/java/pe/com/profind/activities/MainActivity.kt
+++ app/src/main/java/pe/com/profind/activities/MainActivity.kt
@@ -2,12 +2,51 @@ package pe.com.profind.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.exceptions.Exceptions
+import io.reactivex.internal.schedulers.IoScheduler
+import kotlinx.android.synthetic.main.activity_main.*
 import pe.com.profind.R
+import pe.com.profind.adapters.TutorAdapter
+import pe.com.profind.models.TutorInterface
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        rvTutors.layoutManager =  LinearLayoutManager(this)
+
+        getTutors()
+    }
+
+    private fun getTutors() {
+        val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .baseUrl("http://tutorapp.somee.com/api/").build()
+
+        val postsApi = retrofit.create(TutorInterface::class.java)
+
+        var response = postsApi.getAllTutors()
+
+        response.observeOn(AndroidSchedulers.mainThread()).subscribeOn(IoScheduler()).subscribe(
+            {   rvTutors.adapter = TutorAdapter(it, this)
+                // no op
+            },
+            { throwable ->
+                // throw new RuntimeException("Error observing strings", throwable);
+                // instead of throwing, just propagate
+                Exceptions.propagate(throwable)
+            })
+
+
+
     }
 }
