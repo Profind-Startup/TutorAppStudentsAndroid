6cb90d8e88560fb68ba6f899ccb7516074e54198
u201314147 <u201314147@upc.edu.pe>
2019-10-22 23:25:12 -0500

    reservas opciones

---
+++ app/src/main/java/pe/com/profind/models/Subject.kt
@@ -0,0 +1,11 @@
+package pe.com.profind.models
+
+import com.google.gson.annotations.SerializedName
+
+
+
+data class Subject(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("area") val area: String
+)
